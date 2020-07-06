4284d321556ef3fb71f22c26bac169501105b544
Drew Han <drewhangar@gmail.com>
2020-07-06 19:49:37 +0000

    Homepage update

--- src/gatsby-theme-carbon/components/Header.js
+++ src/gatsby-theme-carbon/components/Header.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
-import { Link } from 'gatsby';
+import React, { useContext } from 'react'
+import { Link } from 'gatsby'
 import {
   Header as ShellHeader,
   HeaderGlobalBar,
   HeaderGlobalAction,
-} from 'carbon-components-react';
+} from 'carbon-components-react'
 
 import {
   header,
@@ -12,77 +12,43 @@ import {
   navLink,
   navLists,
   navLeftBorder,
-
   switcherButtonOpen,
   headerButton,
   switcherButton,
-
   globalBar,
-} from './Header.module.scss';
-import { AppSwitcher20, Close20 } from '@carbon/icons-react';
-import cx from 'classnames';
-import NavContext from 'gatsby-theme-carbon/src/util/context/NavContext';
-import { Button } from 'carbon-components-react'
-import { useAuth } from "react-use-auth";
+} from './Header.module.scss'
+import { AppSwitcher20, Close20 } from '@carbon/icons-react'
+import cx from 'classnames'
+import NavContext from 'gatsby-theme-carbon/src/util/context/NavContext'
 
 const Header = ({ children }) => {
-  const {
-    toggleNavState,
-    switcherIsOpen,
-  } = useContext(NavContext);
-  const { isAuthenticated, login, logout } = useAuth();
+  const { toggleNavState, switcherIsOpen } = useContext(NavContext)
 
   return (
     <>
       <ShellHeader aria-label="Header" className={header}>
-        <Link
-          className={headerName}
-          to="/"
-        >
+        <Link className={headerName} to="/">
           Freight Trust
         </Link>
         <div className={navLists}>
-          {/* <div className={navLeftBorder}></div> */}
-          <Link
-            className={navLink}
-            to="/freight"
-          >
+          <div className={navLeftBorder}></div>
+          <Link className={navLink} to="/freight">
             Products
           </Link>
-          <Link
-            className={navLink}
-            to="/"
-          >
+          <Link className={navLink} to="/">
             Solutions
           </Link>
-          <Link
-            className={navLink}
-            to="/"
-          >
+          <Link className={navLink} to="/">
             Partners
           </Link>
-          <Link
-            className={navLink}
-            to="/"
-          >
+          <Link className={navLink} to="/">
             Marketplace
           </Link>
-          <Link
-            className={navLink}
-            to="/"
-          >
+          <Link className={navLink} to="/">
             Pricing
           </Link>
-          <div
-            className={navLink}
-          >
-            {
-              isAuthenticated() ? <Button kind="primary" size='field' onClick={logout}>SIGN OUT</Button> :
-              <Button kind="primary" size='field' onClick={login}>SIGN IN</Button>
-            }
-          </div>
         </div>
-        
+
         <HeaderGlobalBar className={globalBar}>
           <HeaderGlobalAction
             className={cx(headerButton, switcherButton, {
@@ -90,28 +56,27 @@ const Header = ({ children }) => {
             })}
             aria-label="Switch"
             onClick={() => {
-              toggleNavState('switcherIsOpen');
-              toggleNavState('searchIsOpen', 'close');
-              toggleNavState('leftNavIsOpen', 'close');
+              toggleNavState('switcherIsOpen')
+              toggleNavState('searchIsOpen', 'close')
+              toggleNavState('leftNavIsOpen', 'close')
             }}
           >
             {switcherIsOpen ? <Close20 /> : <AppSwitcher20 />}
           </HeaderGlobalAction>
         </HeaderGlobalBar>
-
       </ShellHeader>
     </>
-  );
-};
+  )
+}
 
 const DefaultHeaderText = () => (
   <>
     Freight Trust<span>& Clearing</span>
   </>
-);
+)
 
 Header.defaultProps = {
   children: <DefaultHeaderText />,
-};
+}
 
-export default Header;
+export default Header
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { FourOhFour } from 'gatsby-theme-carbon';
+import React from 'react'
+import { FourOhFour } from 'gatsby-theme-carbon'
 
 const links = [
   { href: '/components/markdown', text: 'Markdown' },
   { href: '/components/Aside', text: 'Aside' },
   { href: '/components/demo', text: 'Demo' },
-];
+]
 
-const Custom404 = () => <FourOhFour links={links} />;
+const Custom404 = () => <FourOhFour links={links} />
 
-export default Custom404;
+export default Custom404
