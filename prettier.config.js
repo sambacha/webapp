875f79180453154c75ab207f3cb935d103578b16
sam <sam@freighttrust.com>
2020-08-22 20:02:54 -0700

    chore: script:format
    Signed-off-by: sam <sam@freighttrust.com>

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,14 @@
-module.exports = require('prettier-config-carbon')
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright  FreightTrust and Clearing Corporation
+ *
+ * This source code is licensed under the Apache-2.0 license
+ */
+
+module.exports = {
+  jsxBracketSameLine: true,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: "es5",
+  proseWrap: "always",
+};
