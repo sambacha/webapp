457f92d33dc41098477769270383ed91dcf24677
sam bacha <sam@freighttrust.com>
2020-09-03 00:51:50 -0700

    CI/CD Update (#125)

---
+++ series-ci-circleci.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+curl \
+  --header "Authorization: Token ebeb85ba-9108-4295-9377-1e66ad00d58c" \
+  --header "Content-Type: application/json" \
+  --data "{
+    \"values\":[
+      {
+        \"line\":\"a\",
+        \"value\":\"1 %\"
+      },
+      {
+        \"line\":\"b\",
+        \"value\":\"2 %\"
+      },
+      {
+        \"line\":\"c\",
+        \"value\":\"3 %\"
+      }
+    ],
+    \"sha\":\"${CIRCLE_SHA1}\"
+  }" \
+  https://seriesci.com/api/freight-trust/webapp/:series/many
