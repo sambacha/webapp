a69659b2553ac30b006ee8df994a256da22092a8
sam <sam@freighttrust.com>
2020-10-11 11:49:35 -0700

    chore(lint): format prettier with updated rules

--- eslintrc.js
+++ eslintrc.js
@@ -1,28 +1,27 @@
 module.exports = {
-      "extends": ["wesbos", "plugin:mdx/recommended"],
-    "overrides": [{
-        "files": ["*.md"],
-        "rules": {
-          "react/prop-types": 1,
-          "react/destructuring-assignment": 1,
-          "react/no-access-state-in-setstate": 1,
-          "react/jsx-props-no-spreading": 1,
+  extends: ['wesbos', 'plugin:mdx/recommended'],
+  overrides: [
+    {
+      files: ['*.md'],
+      rules: {
+        'react/prop-types': 1,
+        'react/destructuring-assignment': 1,
+        'react/no-access-state-in-setstate': 1,
+        'react/jsx-props-no-spreading': 1,
 
-          "react/jsx-no-useless-fragment": 1,
-          "no-useless-escape": 0,
-          "prettier/prettier": [
-            2,
-            {
-            
-              "parser": "markdown"
-            }
-          ]
-        }
+        'react/jsx-no-useless-fragment': 1,
+        'no-useless-escape': 0,
+        'prettier/prettier': [
+          2,
+          {
+            parser: 'markdown',
+          },
+        ],
       },
-      {
-        "files": ["*.mdx"],
-        "extends": ["plugin:mdx/overrides"]
-      }
-    ]
-  
-}
+    },
+    {
+      files: ['*.mdx'],
+      extends: ['plugin:mdx/overrides'],
+    },
+  ],
+};
